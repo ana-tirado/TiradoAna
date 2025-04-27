aa7fc44597fecba26aecc8c2236b8166342218d4
Ana Tirado Fernández <anatiradofdez@gmail.com>
2025-04-27 22:43:39 +0200

    Primer commit: añadir practica-tirado.php

---
+++ practica-tirado.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Suma dos números
+ *
+ * @param int $a Primer número
+ * @param int $b Segundo número
+ * @return int Resultado de la suma
+ * @author Ana Tirado Fernández
+ * @version 1.0
+ * @internal Solo para uso interno.
+ */
+function sumar($a, $b) {
+	return $a + $b;
+}
+
+/**
+ * Resta dos números
+ *
+ * @param int $a Minuendo
+ * @param int $b Sustraendo
+ * @return int Resultado de la resta
+ * @author Ana Tirado Fernández
+ * @version 1.0
+ */
+function restar($a, $b) {
+	return $a - $b;
+}
+?>
